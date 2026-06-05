ba061f05c61c16af08d414ac8ffb2752d0173876
Vitus <83448449+vitustockholm@users.noreply.github.com>
2026-06-05 13:42:27 -0700

    Add Aave V3 flashloan interface

---
+++ contracts/interfaces/IAaveV3Pool.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+interface IAaveV3Pool {
+    function flashLoanSimple(
+        address receiverAddress,
+        address asset,
+        uint256 amount,
+        bytes calldata params,
+        uint16 referralCode
+    ) external;
+}
