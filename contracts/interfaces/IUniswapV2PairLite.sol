aea3961de18d9cdae330125eb4860029cb822a6e
Vitus <83448449+vitustockholm@users.noreply.github.com>
2026-06-05 13:49:36 -0700

    Add lightweight Uniswap pair interface

---
+++ contracts/interfaces/IUniswapV2PairLite.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+interface IUniswapV2PairLite {
+    function token0() external view returns (address);
+    function token1() external view returns (address);
+}
